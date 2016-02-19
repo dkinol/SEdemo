ca5a4175c93784de30479849f7c7fb0b6333baf7
Brian Esch <eschbri@umich.edu>
2016-02-19 15:46:32 -0500

    Fixed small bug with support functions

--- controllers/support.py
+++ controllers/support.py
@@ -1,3 +1,5 @@
+from flask import *
+
 ''' This takes a list of error messages'''
 def generate_error_response(errors):
     messages = []
@@ -9,9 +11,9 @@ def generate_error_response(errors):
 def send_401():
 	errors = []
 	errors.append("You do not have the necessary credentials for the resource")
-	return generate_error_response(errors)
+	return jsonify(generate_error_response(errors)), 401
 
 def send_403():
 	errors = []
 	errors.append("You do not have the necessary permissions for the resource")
-	return generate_error_response(errors)
+	return jsonify(generate_error_response(errors)), 403
