ba8b673b63e87b55232a4f29dab353284dca1dd0
Brian Esch <eschbri@umich.edu>
2016-02-18 23:50:06 -0500

    Made album and pic behave like Single Page Application

---
+++ static/js/router.js
@@ -0,0 +1,22 @@
+function get_base_route(){
+	return '';
+}
+function album_api_route(id){
+	return get_base_route() + '/api/v1/album/' + id;
+}
+
+function pic_api_route(id){
+	return get_base_route() + '/api/v1/pic/' + id;
+}
+
+function static_image_route(id, format){
+	return get_base_route() + '/static/images/' + id + '.' + format;
+}
+
+function pic_template_route(id){
+	return get_base_route() + '/pic?id=' + id;
+}
+
+function album_template_route(id){
+	return get_base_route() + '/album?id=' + id;
+}
---
+++ static/js/spa.js
@@ -0,0 +1,156 @@
+// Sets the basic page contents for an album
+function set_album_context(){
+		$("#content").empty();
+		$("#content").append("<h1>This is a view for a single album, a user cannot edit</h1>");
+		$("#content").append("<h2 id=album_title class=text-center></h2>");
+		$("#content").append("<ul id=album_list></ul>");
+}
+
+// Sets the basic page contents for a picture
+function set_pic_context(picid){
+	$("#content").empty();
+	$("#content").append('<img id=full_pic />'); 
+	$("#content").append('<p id=pic_' + picid+ '_caption />');
+	$("#content").append('<a id=prev_pic />');
+	$("#content").append('<a id=parent_album />'); 
+	$("#content").append('<a id=next_pic />');
+} 
+
+// Thumbnail templage for a picture in /album
+function thumb_pic_template(picid, picformat){
+	return '<div class=row>' + 
+		'<a id=pic_' + picid + '_link class="btn btn-primary" target=' + picid + '>View</a>' +
+		'<img src="' + static_image_route(picid, picformat) + '" width=128 height=128></div>';
+}
+
+// Function meant for displaying an album
+function displayAlbum(album){
+	$("#album_title").val(album.title);
+	for (i = 0; i < album.pics.length; ++i){
+		var new_ele = thumb_pic_template(album.pics[i].picid, album.pics[i].format);
+		$("#album_list").append("<li>Picture: " + new_ele + "</li>");
+	}
+}
+
+// Fetches and displays album from server
+function get_and_display_album(albumid){
+	$.ajax({
+		url: album_api_route(albumid),
+		type: "GET",
+		success: function(result){
+			var Album = result;
+			console.log(Album);
+			displayAlbum(Album);
+		}
+	});
+}
+
+// Represents a model for a picture
+var PicModel = {
+	albumid: "",
+	caption: "",
+	format: "",
+	next: "",
+	picid: "",
+	prev: ""
+};
+
+/// Templating functions for pictures
+function full_pic_template(src){
+	return '<img src="'+src+'" style="width:100%;height=100%;" alt="" />'; 
+}
+function comment_template(pic_id, pic_comment){
+	return '<p class="text-center" id="'+pic_id+'" >'+pic_comment+'</p>'; 
+}
+function prev_pic_template(pic_url, picid){
+	return '<a id="prev_pic" type="button" class="btn btn-default" href="'+pic_url+'" target="' + picid + '">Previous</a>'; 
+}
+function parent_album_template(album_url){
+	return '<a id="parent_album" type="button" class="btn btn-default" href="'+album_url+'">Return to Album</a>';
+}
+function next_pic_template(pic_url, picid){
+	return '<a id="next_pic" type="button" class="btn btn-default" href="'+pic_url+'" target="' + picid + '">Next</a>'; 
+}
+
+// Function takes a picture and displays it
+function displayPicture(photo){
+	$("#content").empty();
+	$("#content").append(full_pic_template(static_image_route(photo.picid, photo.format)));
+	$("#content").append(comment_template("pic_"+photo.picid+"_caption", photo.caption));
+	if (photo.prev != ""){
+		$("#content").append(prev_pic_template(pic_template_route(photo.prev), photo.prev));
+	}
+	$("#content").append(parent_album_template(album_template_route(photo.albumid)));
+	if (photo.next != "") {
+		$("#content").append(next_pic_template(pic_template_route(photo.next), photo.next)); 
+	}
+}
+
+// Function gets picture from server then displays it
+function get_and_display_pic(inpicid){
+	$.ajax({
+		url: pic_api_route(inpicid), 
+		type: "GET", 
+		success: function(result) {
+			PicModel = result; 
+			console.log(PicModel); 
+			displayPicture(PicModel); 
+		}
+	}); 
+}
+
+// Album page, when clicked it loads a picture
+$('body').on('click', "a[id^='pic_']", function(event){
+		// Prevents the user from following the link
+		event.preventDefault();
+		var picId = event.target.target;
+		
+		$.ajax({
+			url: pic_api_route(picId),
+			type: "GET",
+			success: function(result){
+				set_pic_context(picId);
+				get_and_display_pic(picId);
+			}
+		});
+});
+
+// Controller functions for next and previous pic
+$('body').on('click', "#prev_pic", function(event){
+	event.preventDefault();
+	var picid = event.target.target;
+	get_and_display_pic(picid);
+});
+
+$('body').on('click', "#next_pic", function(event){
+	event.preventDefault();
+	var picid = event.target.target;
+	get_and_display_pic(picid);
+});
+
+$('body').on('click', "#parent_album", function(event){
+	event.preventDefault();
+	set_album_context();
+	get_and_display_album(PicModel.albumid);
+});
+
+// Controller function for editing caption
+$("#new_cap").submit(function(event){
+	// Prevents the form from submitting normally
+	event.preventDefault();
+	PicModel.caption = $("#pic_caption_input").val();
+	$.ajax({
+		url: pic_api_route(PicModel.picid),
+		type: 'PUT',
+		contentType: "application/json",
+		data: JSON.stringify(PicModel),
+		success: function(response){
+			console.log(response);
+			$("#pic_caption_input").val("");
+			$('#pic_' + PicModel.picid + '_caption').val(PicModel.caption);
+		},
+		error: function(e){
+			console.log(e.message);
+		}
+	});
+});
